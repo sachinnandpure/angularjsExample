d8e2943d6c1aa6d22144495144c6ecdd1407c323
sachinnandpure <saccreation22@gmail.com>
2024-12-28 12:14:24 +0530

    module1 assignment done

--- ModuleSolution1/app.js
+++ ModuleSolution1/app.js
@@ -1,24 +1,30 @@
-var app = angualr.module('myApp',[]);
+var app = angular.module('myApp',[]);
 app.controller("MyController", function($scope){
     $scope.getInputName;
     $scope.message;
-    $scope.checkName=function(){
+    $scope.alert="text-info"
+    $scope.checkLunch=function(){
         if($scope.getInputName){
             var dishesCount = $scope.getInputName.split(",").length;
             if($scope.getInputName.split(",").every(str => str==="")){
+                $scope.message='';
                 alert("Empty string not allowed");
+              
             }
             else {
                 if(dishesCount>3){
                     $scope.message ="Too much!";
+                    $scope.alert = "text-info";
                 }
                 else if(dishesCount <=3 && dishesCount){
                     $scope.message="Enjoy!";
+                    $scope.alert = "text-info";
                 }
             }
         }
         else {
             $scope.message =" Please enter data first";
+            $scope.alert = "text-danger";
         }
     }
 });
